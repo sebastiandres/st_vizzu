500f97b923efb10d3a0a37d6daf9275734160aaf
sebastiandres <sebastiandres@gmail.com>
2022-09-22 15:31:03 -0300

    more examples

---
+++ pages/simple.py
@@ -0,0 +1,26 @@
+from ipyvizzu import Data, Config
+from ipyvizzustory import Story, Slide, Step
+
+
+data = Data()
+data.add_series("Foo", ["Alice", "Bob", "Ted"])
+data.add_series("Bar", [15, 32, 12])
+data.add_series("Baz", [5, 3, 2])
+
+story = Story(data=data)
+
+slide1 = Slide(
+    Step(
+        Config({"x": "Foo", "y": "Bar"}),
+    )
+)
+story.add_slide(slide1)
+
+slide2 = Slide(
+    Step(
+        Config({"color": "Foo", "x": "Baz", "geometry": "circle"}),
+    )
+)
+story.add_slide(slide2)
+
+story.play()
